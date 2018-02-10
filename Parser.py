27553ffab861405fee150b0b889faa0374ad59b7
bzdvdn <bzdv.dn@gmail.com>
2018-02-10 15:37:22 +0300

    fix prasers for correcting data

--- Parser.py
+++ Parser.py
@@ -87,78 +87,6 @@ class Parser(object):
 				#print(json.dumps(data,indent=2, ensure_ascii=False))
 			else:
 				continue
-
-
-	
-			
-
-def read_file(filename):
-	with open(filename, 'r') as f:
-		return f.read().split('\n')
-
-
-class RabotauaParser(Parser):
-
-	def get_total_pages(self, html):
-		soup = BeautifulSoup(html, "lxml")
-		page = soup.find_all('tr')[-1].find('td').find('dl').find_all('dd')[-2].find('a').get('href')
-		total_pages = page.split('=')[-1]
-		return int(total_pages)
-
-	def get_pages_data(self, html):
-		soup = BeautifulSoup(html, "lxml")
-		ads = soup.find('table', class_='f-vacancylist-tablewrap').find_all('tr')[0:-1]
-
-		for index, iterator in enumerate(ads, start=1):
-			name = soup.find('tr').find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').text.strip()
-
-			try:
-				title = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').text.strip()
-				company = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('a', class_='f-text-dark-bluegray f-visited-enable').text.strip()
-				city = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('div', class_='f-vacancylist-characs-block fd-f-left-middle').find('p', class_='fd-merchant').text.strip()
-				url = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').find('a').get('href')
-				print('{} - index, url - {}'.format(index,url))
-			except Exception as e:
-				print('{} - index'.format(index))
-				print(e)				
-			try:
-				page = self.get_html('https://rabota.ua' + url, useragent)
-				desc_soup = BeautifulSoup(page, "lxml")
-			except Exception as e:
-				print(e)
-			try:
-				employment = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[2].text
-			except:
-				print('employment')
-				employment = '----'
-
-			try:
-				skills = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[4].text
-			except:
-				print('skill')
-				skills = ''
-
-			try:
-				mb_skills = skills = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[5].text
-			except:
-				print('mb_skill')
-				mb_skills = ''
-
-			data = {
-					'title': title,
-					'company': company,
-					'city': city,
-					'employment': employment,
-					'skills': skills,
-					'mb_skills': mb_skills,
-					'url': url	
-																
-				}
-
-			self.write_csv(data, str(self.message), str(self.chat_id))
-			#print(json.dumps(data,indent=2, ensure_ascii=False))
-
-
 class HHruParser(Parser):
 
 	def get_total_pages(self, html):
@@ -222,6 +150,73 @@ class HHruParser(Parser):
 				continue
 
 
+class RabotauaParser(Parser):
+
+	def get_total_pages(self, html):
+		soup = BeautifulSoup(html, "lxml")
+		page = soup.find_all('tr')[-1].find('td').find('dl').find_all('dd')[-2].find('a').get('href')
+		total_pages = page.split('=')[-1]
+		return int(total_pages)
+
+	def get_pages_data(self, html):
+		soup = BeautifulSoup(html, "lxml")
+		ads = soup.find('table', class_='f-vacancylist-tablewrap').find_all('tr')[0:-1]
+
+		for index, iterator in enumerate(ads, start=1):
+			name = soup.find('tr').find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').text.strip()
+
+			try:
+				title = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').text.strip()
+				company = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('a', class_='f-text-dark-bluegray f-visited-enable').text.strip()
+				city = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('div', class_='f-vacancylist-characs-block fd-f-left-middle').find('p', class_='fd-merchant').text.strip()
+				url = iterator.find('td').find('article', class_='f-vacancylist-vacancyblock').find('div',class_='fd-f-left').find('div', class_='fd-f1').find('h3').find('a').get('href')
+				print('{} - index, url - {}'.format(index,url))
+			except Exception as e:
+				print('{} - index'.format(index))
+				print(e)				
+			try:
+				page = self.get_html('https://rabota.ua' + url, useragent)
+				desc_soup = BeautifulSoup(page, "lxml")
+			except Exception as e:
+				print(e)
+			try:
+				employment = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[2].text
+			except:
+				print('employment')
+				employment = '----'
+
+			try:
+				skills = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[4].text
+			except:
+				print('skill')
+				skills = ''
+
+			try:
+				mb_skills = skills = desc_soup.find('div', class_='d_content').find('div', class_='d_des').find_all('ul')[5].text
+			except:
+				print('mb_skill')
+				mb_skills = ''
+
+			data = {
+					'title': title,
+					'company': company,
+					'city': city,
+					'employment': employment,
+					'skills': skills,
+					'mb_skills': mb_skills,
+					'url': url	
+																
+				}
+
+			self.write_csv(data, str(self.message), str(self.chat_id))
+			#print(json.dumps(data,indent=2, ensure_ascii=False))
+	
+			
+
+def read_file(filename):
+	with open(filename, 'r') as f:
+		return f.read().split('\n')
+
 
 useragent = {'User-Agent': choice(read_file('useragent.txt'))}
 
@@ -237,6 +232,24 @@ def workparse(msg,chat_id):
 			url_gen = base_url + page + str(i)
 			html = p.get_html(url_gen, useragent)
 			p.get_pages_data(html)
+	
+
+
+def hhru_parse(msg, chat_id):
+		p = HHruParser(msg,chat_id)
+		url = 'https://m.hh.ru/vacancies?text=' + str(p.message)
+		base_url = 'https://m.hh.ru/vacancies?text=' + str(p.message) 
+		page = '&page='
+		total_pages = p.get_total_pages(p.get_html(url,useragent))
+		
+
+		for i in range(1, total_pages+1):
+			url_gen = base_url + page + str(i)
+			html = p.get_html(url_gen, useragent)
+			p.get_pages_data(html)
+
+
+
 
 def rabotaua_parser(msg, chat_id):
 		p = RabotauaParser(msg,chat_id)
@@ -251,21 +264,6 @@ def rabotaua_parser(msg, chat_id):
 			html = p.get_html(url_gen, useragent)
 			p.get_pages_data(html)
 
-
-def rabotaua_parser(msg, chat_id):
-		p = RabotauaParser(msg,chat_id)
-		url = 'https://rabota.ua/jobsearch/vacancy_list?keyWords=' + str(p.message)
-		base_url = 'https://rabota.ua/jobsearch/vacancy_list?keyWords=' + str(p.message) 
-		page = '&pg='
-		total_pages = p.get_total_pages(p.get_html(url,useragent))
-		
-
-		for i in range(1, 5):
-			url_gen = base_url + page + str(i)
-			html = p.get_html(url_gen, useragent)
-			p.get_pages_data(html)
 
 if __name__ == '__main__':
 	rabotaua_parser('python', '2335345')
-
-
--- main.py
+++ main.py
@@ -13,10 +13,11 @@ __author__ = 'bzdvdn'
 
 
 bot = telebot.TeleBot(config.token)
-# bot.remove_webhook()
-# bot.set_webhook(url='https://91967212.ngrok.io')
 
-#print(bot.get_me())
+bot.remove_webhook()
+bot.set_webhook(url='https://97104062.ngrok.io')
+
+print(bot.get_me())
 
 
 
@@ -24,14 +25,11 @@ def delete_file(filename):
 	os.remove(filename)
 
 
-def work_parser(message,parser):
-	if not message.text == '/start':		
+def work_parser(message,parser):		
 		file = open(str(message.from_user.id) + '_-_' + str(message.text) + '.csv', 'rb')
-		bot.send_message(message.from_user.id, 'parse done!')
 		bot.send_document(message.from_user.id, file)
 		delete_file(str(message.from_user.id)  + '_-_' + str(message.text) + '.csv')
-	else:
-		bot.register_next_step_handler(parser, start_command)
+		
 
 
 
@@ -51,16 +49,15 @@ def index():
 	return '<h1> Bot welcomes you</h1>'
 
 
-bot.remove_webhook()
-bot.set_webhook(url='https://8521b153.ngrok.io')
+
 
 @bot.message_handler(commands=['help'])
 def handle_text(message):
 	bot.send_message(message.chat.id, """
 		Этот бот парсит сайты(work.ua, rabota.ua, hh.ru):
-		на предмет поиска работы и выводит вам ссылку на спарсенный ресурс.
+		на предмет поиска работы и выводит вам сsv файл.
 	команды /work_ua, /hh_ru, /rabota_ua
-		после нажатия команд нееобходимо написать слово:
+		после нажатия команд нееобходимо выбрать слово из предложенных на клавиатуре:
 		по какому критерию вы хотите спарсить сайт. Например Python
 
 		""")
@@ -80,26 +77,43 @@ def work_ua_command(message):
 	user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
 	user_markup.row('python', 'ruby','php')
 	user_markup.row('1с', 'front-end', '/start')
-	bot.send_message(message.from_user.id, 'Выберите критерии для парсинга:', reply_markup=user_markup)
+	msg = bot.send_message(message.from_user.id, 'Выберите критерии для парсинга:', reply_markup=user_markup)
+	
 	if message.text == '/work_ua':
-		@bot.message_handler(content_types=['text'])
-		def handle_parser(message):
-			bot.send_message(message.from_user.id, 'Waiting for parsing data...')	
-			parser = workparse(message.text, message.from_user.id)
-			work_parser(message,parser)
+		bot.register_next_step_handler(msg, work_parser)
 	elif message.text == '/hh_ru':
-		@bot.message_handler(content_types=['text'])
-		def handle_parser(message):
-			bot.send_message(message.from_user.id, 'Waiting for parsing data...')	
-			parser = hhru_parse(message.text, message.from_user.id)
-			work_parser(message,parser)
-
+		bot.register_next_step_handler(msg, hh_parser)
 	elif message.text == '/rabota_ua':
-		@bot.message_handler(content_types=['text'])
-		def handle_parser(message):
-			bot.send_message(message.from_user.id, 'Waiting for parsing data...')   
-			parser = rabotaua_parser(message.text, message.from_user.id)
-			work_parser(message,parser)
+		bot.register_next_step_handler(msg, rabota_parser)
+		
+
+
+def work_parser(message):
+	bot.send_message(message.from_user.id, 'Данные парсятся, это может занять некоторое время....1')	
+	parser = workparse(message.text, message.from_user.id)			
+	file = open(str(message.from_user.id) + '_-_' + str(message.text) + '.csv', 'rb')
+	bot.send_document(message.from_user.id, file)
+	delete_file(str(message.from_user.id)  + '_-_' + str(message.text) + '.csv')
+	msg = bot.send_message(message.from_user.id, 'Готово! Для дальнейшей работы нажмите "/start"')
+
+
+def hh_parser(message):
+	bot.send_message(message.from_user.id, 'Данные парсятся, это может занять некоторое время....2')	
+	parser = hhru_parse(message.text, message.from_user.id)			
+	file = open(str(message.from_user.id) + '_-_' + str(message.text) + '.csv', 'rb')
+	bot.send_document(message.from_user.id, file)
+	delete_file(str(message.from_user.id)  + '_-_' + str(message.text) + '.csv')
+	msg = bot.send_message(message.from_user.id, 'Готово! Для дальнейшей работы нажмите "/start"')
+
+def rabota_parser(message):
+	bot.send_message(message.from_user.id, 'Данные парсятся, это может занять некоторое время....3')	
+	parser = rabotaua_parser(message.text, message.from_user.id)			
+	file = open(str(message.from_user.id) + '_-_' + str(message.text) + '.csv', 'rb')
+	bot.send_document(message.from_user.id, file)
+	delete_file(str(message.from_user.id)  + '_-_' + str(message.text) + '.csv')
+	msg = bot.send_message(message.from_user.id, 'Готово! Для дальнейшей работы нажмите "/start"')
+
+
 
 
 
